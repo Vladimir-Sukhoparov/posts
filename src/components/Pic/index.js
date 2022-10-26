ea5ccef6659465227ed727f5550c151bec739650
Vladimir-Sukhoparov <sukhoparov.v.n@gmail.com>
2022-10-26 18:52:52 +0500

    update random_pic#2

--- src/components/Pic/index.js
+++ src/components/Pic/index.js
@@ -1,3 +1,4 @@
+import { CircularProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
@@ -10,24 +11,24 @@ export const Pic = () => {
     setUrlPic(null);
     fetch("https://picsum.photos/800/600").then((res) => setUrlPic(res.url));
   };
-  
+
   useEffect(() => {
     console.log(urlPic);
-
   }, [urlPic]);
-
-
-  //добавить иконку из миюай
 
   return (
     <div className="App-pic">
       <Link to="/pic">
-        <img
-          className="pic"
-          src={urlPic}
-          onClick={handleClick}
-          alt='download...'
-        />
+        {!urlPic ? (
+          <CircularProgress color="inherit" />
+        ) : (
+          <img
+            className="pic"
+            src={urlPic}
+            onClick={handleClick}
+            alt="download"
+          />
+        )}
       </Link>
     </div>
   );
